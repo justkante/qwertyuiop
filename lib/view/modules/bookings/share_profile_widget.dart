8c75a88d7408ba7e663e97e55a4c7e10e3262150
justkante <135815502+justkante@users.noreply.github.com>
2026-09-14 12:06:15 -0600

    Fix: Missing imports causing iOS build failure

--- lib/view/modules/bookings/share_profile_widget.dart
+++ lib/view/modules/bookings/share_profile_widget.dart
@@ -9,6 +9,7 @@ import 'package:creatify_mobile/view/theme/theme_extensions.dart';
 import 'package:creatify_mobile/view/utils/app_images.dart';
 import 'package:creatify_mobile/view/utils/extensions.dart';
 import 'package:creatify_mobile/view/widgets/buttons.dart';
+import 'package:creatify_mobile/view/widgets/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
